6e981f04f39501ce04e454865877b750a7d0a0c6
Corneliu Duplachi <corneliu.duplachi@ellation.com>
2019-09-12 15:45:33 +0300

    Renamed junit5 extension

--- src/com/github/dlenroc/restassured/testrail/logger/TestRailJunit5Extension.kt
+++ src/com/github/dlenroc/restassured/testrail/logger/TestRailJunit5Extension.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
-class TestRailJunit5Rule : BeforeTestExecutionCallback, AfterTestExecutionCallback {
+class TestRailJunit5Extension : BeforeTestExecutionCallback, AfterTestExecutionCallback {
     private val testRailFilter = TestRailFilter()
 
     override fun beforeTestExecution(context: ExtensionContext) {
